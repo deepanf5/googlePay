9bb46cc057c78cf3e2e458509bb7f78fa529e220
deepan <deepanappu1999@gmail.com>
2025-05-06 21:06:57 +0530

    adding the google pay component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GooglePayComponent } from './components/google-pay/google-pay.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,GooglePayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
